116d46b3cb72f9f76648674e5b6ad51c583d9afc
Júlia Bueno Seganfredo <150600044+jb-seganfredo@users.noreply.github.com>
2024-09-07 18:23:54 -0300

    Exercícios 1 e 2

--- 1-manipulação-de-objetos/ex01.js
+++ 1-manipulação-de-objetos/ex01.js
@@ -1,2 +1,14 @@
 // 1. Acessando Propriedades de Objetos
 // Objetivo: Crie um objeto carro com propriedades como marca, modelo, ano, e cor. Use for in para iterar sobre as e exibir seus valores no console.
+
+let carro = {
+    marca: 'Chevrolet',
+    modelo: 'Cruze',
+    ano: 2019,
+    cor: 'preto'
+}
+
+for (let prop in carro) {
+    console.log(`${prop}: ${carro[prop]}`)
+}   
+
--- 1-manipulação-de-objetos/ex02.js
+++ 1-manipulação-de-objetos/ex02.js
@@ -1,2 +1,25 @@
 // 2. Verificando Propriedades
 // Objetivo: Crie um objeto livro com propriedades titulo, autor, anoPublicacao e genero. Verifique se a propriedade editora existe no objeto usando for in. Se não existir, adicione essa propriedade ao objeto.
+
+let livro = {
+    titulo: 'O Senhor dos Anéis',
+    autor: 'J.R.R. Tolkien',
+    anoPublicacao: 1954,
+    genero: 'Fantasia'
+}
+
+let editoraExiste = false
+
+for (let prop in livro) {
+    if (prop === 'editora') {
+        editoraExiste = true
+    }
+}
+
+if (!editoraExiste) {
+    livro.editora = 'HarperCollins'
+}
+
+console.log(livro)
+
+    
